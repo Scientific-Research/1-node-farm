ce46659b7081d6ba21e75e16a31bb9a602c300ae
Maximilian <scientificresearch309@gmail.com>
2025-01-15 01:07:33 +0100

    write the very first Node program and run it using node

---
+++ index.js
@@ -0,0 +1,4 @@
+import fs from "fs"; // fs gives us a lot of functions and methods that we can use them here => The NODE is based on the different modules!
+
+const hello = "hello world";
+console.log(hello);
