6124f686e3b050b3bb7b0f6ac38e6680865567e7
Maximilian <scientificresearch309@gmail.com>
2025-01-16 22:19:33 +0100

    reading the text from a file using fs.readFileSync()

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ const hello = "hello world";
 console.log(hello);
 
 // How to write data into files and how to write the data from the files using 'fs' module in NODE:
+// first is the path to the file that we want to read from that:
+const outputAsNumbers = fs.readFileSync("./txt/input.txt"); // without assign the second parameter, it will gives us only buffers which are the numbers and we don't want them!
+
+// Assigning the second parameter as "utf-8", we will have the TEXT as we expected!
+const outputAsText = fs.readFileSync("./txt/input.txt", "utf-8");
+console.log(outputAsText);
