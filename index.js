e8c030ad6b04f045e0ca1d1eed75145e6de2a981
Maximilian <scientificresearch309@gmail.com>
2025-01-17 20:42:35 +0100

    reading file but in a Asynchronous way

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import fs from "fs"; // fs gives us a lot of functions and methods that we can u
 const hello = "hello world";
 console.log(hello);
 
+// Blocking, Synchronous way
+///////////////////////////////////////////////////////////
 // How to write data into files and how to write the data from the files using 'fs' module in NODE:
 // first is the path to the file that we want to read from that:
 const outputAsNumbers = fs.readFileSync("./txt/input.txt"); // without assign the second parameter, it will gives us only buffers which are the numbers and we don't want them!
@@ -19,3 +21,9 @@ const writeTextToFile = `This is what we know about the Avocado: ${fs.writeFileS
 const writeTextToFile2 = `This is what we know about the Avocado: ${outputAsText}.\nCreated on ${new Date().toLocaleString()}`;
 
 fs.writeFileSync("./txt/addEtwas.txt", writeTextToFile2);
+///////////////////////////////////////////////////////////
+
+// Non-Blocking, Asynchronous way
+fs.readFile("./txt/start.txt", () => {
+  console.log("Reading Finished!");
+});
