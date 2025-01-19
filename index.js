5034b923df87b6ce51a2597b920c7081c86d0a8e
Maximilian <scientificresearch309@gmail.com>
2025-01-19 14:21:35 +0100

    replace template function is there

--- index.js
+++ index.js
@@ -15,6 +15,22 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const objectData = JSON.parse(data); // convert the JSON string to a JS object
 // 1) Create the Server => the callback fired off each time a new request hits the server:
 
+// const cardsHtml = objectData.map((el) => replaceTemplate(tempCard, el));
+
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic)
+    output = output.temp.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+};
+
 const tempCard = fs.readFileSync(
   `${__dirname}/templates/template-card.html`,
   "utf-8"
